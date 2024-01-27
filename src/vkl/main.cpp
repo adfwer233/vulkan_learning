83137420c02aadb4e505b2044b626a4f5484c492
Bao Anchang <ba123698745@126.com>
2024-01-27 11:01:24 +0800

    feat: add vkl project and homepage

---
+++ src/vkl/main.cpp
@@ -0,0 +1,32 @@
+#include "iostream"
+
+/**
+ * \mainpage Learning Vulkan Project
+ *
+ * # Introduction
+ *
+ * The aim of this project is learning vulkan programming, from graphics pipeline to parallel programming using compute
+ * shader.
+ *
+ * # Subpages
+ * - \subpage createWindowPage "Creating a window"
+ *
+ * # Note pages
+ * - \subpage notePage1 "Testing Note Page"
+ */
+
+/**
+ * \page notePage1 Note Page1
+ *
+ * This is note page
+ */
+
+/**
+ * \page createWindowPage Creating a window
+ *
+ * Creating a window introduction
+ */
+
+int main() {
+
+}
