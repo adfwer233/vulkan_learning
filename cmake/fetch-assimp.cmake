e19768caa241ee6dfb90d236eb2e0f10a65c0e4e
Bao Anchang <ba123698745@126.com>
2024-02-29 23:56:32 +0800

    fix: use my fork assimp

--- cmake/fetch-assimp.cmake
+++ cmake/fetch-assimp.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(
         assimp
-        GIT_REPOSITORY https://github.com/assimp/assimp
+        GIT_REPOSITORY https://github.com/adfwer233/assimp
 )
 
 FetchContent_MakeAvailable(assimp)
