f8b529bd49f10e569152a75504d9c38fb8122df2
Bao Anchang <ba123698745@126.com>
2024-04-13 15:40:41 +0800

    feat(meta): add name of hack for msvc

---
+++ include/meta_programming/static_string.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string_view>
+
+namespace MetaProgramming{
+
+template<char... args>
+struct StaticString {
+    static constexpr const char str[] = {args...};
+    operator const char*() const {return StaticString::str;}
+};
+
+}
--- test/templates/meta_programming_test.cpp
+++ test/templates/meta_programming_test.cpp
@@ -1,1 +1,29 @@
 #include "meta_programming/static_for_loop.hpp"
+#include "meta_programming/name_of.hpp"
+
+#include "gtest/gtest.h"
+
+struct CustomName {
+
+};
+
+enum CustomEnum {
+    testname1,
+    testname2
+};
+
+#if defined(_MSC_VER)
+
+TEST(MetaProgrammingTest, NameOf) {
+    auto tmp = MetaProgramming::type_name_of<CustomName>();
+    std::cout << tmp.str << std::endl;
+    std::cout << MetaProgramming::type_name_of<CustomEnum, testname1>().str << std::endl;
+    std::cout << std::boolalpha;
+    std::cout << MetaProgramming::is_member_of_enum<CustomEnum, static_cast<CustomEnum>(100)>() << std::endl;
+    std::cout << MetaProgramming::is_member_of_enum<CustomEnum, testname1>() << std::endl;
+    for (auto item: MetaProgramming::all_name_of_enum<CustomEnum>())
+        std::cout << item << ' ';
+    std::cout << std::endl;
+}
+
+#endif
